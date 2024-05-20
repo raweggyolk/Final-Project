fc44bbf0bf9fcecbb3295b363d678e36a2fa25e4
BombyDee <Dazhou2@s.sfusd.edu>
2024-05-20 15:30:58 -0700

    Wall Hug Jump

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const wall_jump_pushback = 100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,6 +18,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	handle_jump()
 
 	
 	var direction = Input.get_axis("move_left", "move_right")
@@ -40,3 +42,15 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func handle_jump():
+	if Input.is_action_just_pressed("Jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall() and Input.is_action_pressed("move_right"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = --wall_jump_pushback
+		if is_on_wall() and Input.is_action_pressed("move_left"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = wall_jump_pushback
+		
