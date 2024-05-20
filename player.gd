987e76aa1b37ca3fdbfbbc930dca65dac8792f38
BombyDee <Dazhou2@s.sfusd.edu>
2024-05-20 15:03:12 -0700

    Movement Fail #3

--- player.gd
+++ player.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var coyote_time = 0.1
+var can_jump = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
@@ -26,13 +27,21 @@ func _physics_process(delta):
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
-	if is_on_floor():
+	if can_jump:
 		if direction == 0:
 			animated_sprite.play("Idle")
 		else:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
+		
+		
+	if is_on_floor() and can_jump == false:
+		can_jump = true
+	elif can_jump == true and $CoyoteTimer.is_stopped():
+		$CoyoteTimer.start(coyote_time)
+		
+	
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -40,3 +49,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_coyote_timer_timeout():
+	can_jump = false
+	pass
