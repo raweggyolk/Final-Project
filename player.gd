ec035d892a2eb7d93aa90c3bcdc70c7b9dcaf57e
BombyDee <Dazhou2@s.sfusd.edu>
2024-05-22 10:12:29 -0700

    Double Jump

--- player.gd
+++ player.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
-const wall_jump_pushback = 800
+const JUMP_VELOCITY = -250.0
+const wall_jump_pushback = 500.0
+var is_wall_sliding = true
+var jump_count = 0
+var max_jumps = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,10 +17,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	
+	if is_on_floor():
+		jump_count = 0
+	
+	if Input.is_action_just_pressed("Jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 	handle_jump()
 
 	
@@ -54,3 +60,4 @@ func handle_jump():
 			velocity.y = JUMP_VELOCITY
 			velocity.x = wall_jump_pushback
 		
+
